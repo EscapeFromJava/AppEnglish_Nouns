1b141f1088788c72b05e8c26b2f93531ae8f34fd
EscapeFromJava <reske.artur@gmail.com>
2022-04-24 17:43:05 +0300

    v 1.5 Add: "MainApp" - add icon;

--- src/main/java/com/example/appenglish_nouns/MainApp.java
+++ src/main/java/com/example/appenglish_nouns/MainApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,9 +15,10 @@ public class MainApp extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("maket/main-view.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
-        stage.setTitle("Nouns!");
+        stage.getIcons().add(new Image(MainApp.class.getResourceAsStream("img/icon.png")));
         stage.setScene(scene);
         stage.setResizable(false);
+        stage.setTitle("Nouns!");
         stage.show();
     }
 
